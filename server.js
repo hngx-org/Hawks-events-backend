d097fd79712343b2a3a0c49d45f1cde3b880bd1b
dhamolahedonist <dhamolahedonist>
2023-09-19 22:48:35 +0100

    finished env setup

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require("express")
-const {PORT} = require('./src/config/constants')
+const express = require("express");
+const { PORT } = require("./src/config/constants");
+const cors = require("cors");
 const errorHandler = require("./src/middlewares/error-handler");
 const notFound = require("./src/middlewares/not-found");
-const app = express()
+const app = express();
+require("dotenv").config();
 
-app.use(cors())
-// these already do the work of bodyParser 
-app.use(express.json())
-app.use(express.urlencoded({extended:false})) 
+app.use(cors());
+// these already do the work of bodyParser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(errorHandler)
-app.use(notFound)
+app.use(errorHandler);
+app.use(notFound);
 
 const start = async () => {
-    // bring in the database 
-    const server = app.listen(PORT, () => {
-        console.log(`App started at port: ${PORT}`)
-    })
-}
+  // bring in the database
+  const server = app.listen(PORT, () => {
+    console.log(`App started at port: ${PORT}`);
+  });
+};
 
-start()
+start();
 process.on("unhandledRejection", (err) => {
-    server.close(() => process.exit(1))
-})
+  server.close(() => process.exit(1));
+});
