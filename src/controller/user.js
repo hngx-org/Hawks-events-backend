ea2050b55f24c88803661d1325f4487872d09731
devacunetixtech <adebisiprecious780@gmail.com>
2023-09-20 16:45:58 +0100

    changed to sequelize since its orm

--- src/controller/user.js
+++ src/controller/user.js
@@ -41,21 +41,28 @@ const updateProfile = async (req,res) => {
 const updateGroupDetails = async (req, res) => {
     const { groupId } = req.params;
     const { name, description } = req.body;
-    const userId = req.user.id; // Replace with your authentication mechanism
   
     try {
-      // Check if the group exists and the user is the creator
-      const [groupRows] = await db.query('SELECT * FROM groups WHERE id = ? AND creator_id = ?', [groupId, userId]);
-      if (groupRows.length === 0) {
-        return res.status(404).json({ error: 'Group not found or unauthorized' });
+      const group = await Group.findByPk(groupId);
+  
+      if (!group) {
+        return res.status(404).json({ error: 'Group not found' });
+      }
+  
+      // Check if the user is the owner of the group (you may implement user authentication)
+      if (group.userId !== req.user.id) {
+        return res.status(403).json({ error: 'Unauthorized' });
       }
   
       // Update group details
-      await db.query('UPDATE groups SET name = ?, description = ? WHERE id = ?', [name, description, groupId]);
+      group.name = name || group.name;
+      group.description = description || group.description;
   
-      res.json({ message: 'Group details updated successfully' });
+      await group.save();
+  
+      res.json(group);
     } catch (error) {
-      console.error('Error updating group:', error);
+      console.error(error);
       res.status(500).json({ error: 'Could not update group' });
     }
   };
