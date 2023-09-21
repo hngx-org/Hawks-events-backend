a3cc4d98e9ac7ec048642be09d5ba6bfc8bfcdd0
urizennnnnn <igamerryt@gmail.com>
2023-09-21 16:04:12 +0100

    Add examples on how to use the created errors

--- src/controller/user.js
+++ src/controller/user.js
@@ -4,8 +4,8 @@ const { CustomError } = require('../error/errors')
 const alloha = async (req, res, next) => {
     // return next(CustomError('test', 429)) //use case
 
-    const message = constants.MESSAGES.USER_CREATED
-    return next(CustomError(message,200))
+    // const message = constants.MESSAGES.USER_CREATED
+    // return next(CustomError(message,200))Use case for constants
 
 }
 
