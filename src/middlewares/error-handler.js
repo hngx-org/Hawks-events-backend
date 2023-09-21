47a4395c81c34467f8dc9aa060555f88931c8c86
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2023-09-21 12:55:32 +0100

    modified overall error handler

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 const errorHandler = (err, req, res, next) => {
-    const CustomMessage = {
-      message: err.message || 'Internal Serval Error',
-      statusCode: err.statusCode || 500,
-    };
-    res.status(CustomMessage.statusCode).json({ message: CustomMessage.message });
+  const CustomMessage = {
+    message: err.message || 'Internal Serval Error',
+    statusCode: err.statusCode || 500,
   };
-  
-  module.exports = errorHandler;
-  
+  return res
+    .status(CustomMessage.statusCode)
+    .json({ message: CustomMessage.message });
+};
+
+module.exports = errorHandler;
