d9c4bdfc3025e6dceb4e8a806434ed3ac417ec77
urizennnnnn <igamerryt@gmail.com>
2023-09-21 09:34:56 +0100

    added examples

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,14 +1,18 @@
 const userModel = require('../models/user')
-
+const constants = require('../config/constants')
+const ClassError = require('../error/errors')
 const alloha = async (req,res) => {
-        res.status(200).json({message:"It's still day one!"})
+        // res.status(200).json({message:"It's still day one!"}
+        // throw new ClassError.AcceptedError("It's still day one")
+        // throw new ClassError.CreatedError(constants.MESSAGESS.USER_CREATED)` Using the pre defined message`s
+        
 }
 
 const register = async (req,res) => {
     try {
         
     } catch (error) {
-        
+        throw new ClassError.ServerError('Internal Server Error')
     }
 }
 
