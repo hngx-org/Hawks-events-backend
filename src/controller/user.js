a88c1f75033f706ff7b16ae6081139cc62ac5ca8
Fawaz Oduola <oduolafawaz@gmail.com>
2023-09-21 18:37:43 +0100

    save user in db

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,50 +1,70 @@
-const userModel = require('../models/user')
-const constants = require('../config/constants')
-const { CustomError } = require('../error/errors')
+const userModel = require("../models/user");
+const constants = require("../config/constants");
+const { CustomError } = require("../error/errors");
 const alloha = async (req, res, next) => {
-    // res.status(500).json({ message: constants.MESSAGES.USER_CREATED });
-    return next(CustomError('test', 429)) //use case
-}
+  // res.status(500).json({ message: constants.MESSAGES.USER_CREATED });
+  return next(CustomError("test", 429)); //use case
+};
 
+const register = async (req, res, next) => {
+  const requestBody = req.body || {};
+  const userData = {
+    name: requestBody.name || null,
+    email: requestBody.email || null,
+    avatar: requestBody.avatar || null,
+  };
 
-const register = async (req,res) => {
-    try {
-        
-    } catch (error) {
-        
+  const requiredFields = ["email", "name", "avatar"];
+
+  for (const field of requiredFields) {
+    if (!userData[field]) {
+      res.status(400).json({ error: `Missing ${field}` });
+      return;
     }
-}
+  }
+  let User;
+  let created;
 
+  try {
+    userModel
+      .findOrCreate({
+        where: { email: userData.email },
+        defaults: {
+          name: userData.name,
+          email: userData.email,
+          avatar: userData.avatar,
+        },
+      })
+      .then((data) => {
+        res.status(201).json({ statusCode: 201, message: "user created" });
+      })
+      .catch((error) => {
+        console.log("error");
+        throw new BadRequestError("Invalid user data");
+      });
+  } catch (error) {
+    next(err);
+  }
+};
 
-const login = async (req,res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+const login = async (req, res) => {
+  try {
+  } catch (error) {}
+};
 
+const profile = async (req, res) => {
+  try {
+  } catch (error) {}
+};
 
-const profile = async (req,res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
-
-const updateProfile = async (req,res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
-
+const updateProfile = async (req, res) => {
+  try {
+  } catch (error) {}
+};
 
 module.exports = {
-    alloha,
-    register,
-    login, 
-    profile
-}
+  alloha,
+  register,
+  login,
+  profile,
+};
