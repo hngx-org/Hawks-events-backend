ac9710866b828b769ce09b4e79c5f99ef538c87e
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2023-09-22 11:59:43 +0100

    fixed a bug in register route

--- server.js
+++ server.js
@@ -1,31 +1,28 @@
-const express = require("express");
-const { PORT } = require("./src/config/constants");
-const cors = require("cors");
-const errorHandler = require("./src/middlewares/error-handler");
-const notFound = require("./src/middlewares/not-found");
+const express = require('express');
+const { PORT } = require('./src/config/constants');
+const cors = require('cors');
+const errorHandler = require('./src/middlewares/error-handler');
+const notFound = require('./src/middlewares/not-found');
 const app = express();
-
 
 app.use(cors());
 // these already do the work of bodyParser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 //bring in the routes
-const user = require("./src/routes/user");
-const auth = require("./src/routes/auth");
-const group = require("./src/routes/group");
-const events = require("./src/routes/event");
-const comment = require("./src/routes/comments");
-const upload = require("./src/routes/upload");
+const user = require('./src/routes/user');
+// const auth = require("./src/routes/auth");
+const group = require('./src/routes/group');
+const events = require('./src/routes/event');
+const comment = require('./src/routes/comments');
+const upload = require('./src/routes/upload');
 
-app.use("/api/users", user);
-app.use("/api/events", events);
-app.use("/api/group", group);
+app.use('/api/users', user);
+app.use('/api/events', events);
+app.use('/api/group', group);
 app.use('/api/comment', comment);
-app.use("/api/upload", upload);
-
+app.use('/api/upload', upload);
 
 app.use(errorHandler);
 app.use(notFound);
@@ -34,6 +31,6 @@ const server = app.listen(PORT, () => {
   console.log(`App started at port: ${PORT}`);
 });
 
-process.on("unhandledRejection", (err) => {
-	server.close(() => process.exit(1));
+process.on('unhandledRejection', (err) => {
+  server.close(() => process.exit(1));
 });
