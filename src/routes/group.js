f4a700a58b3363d1838806020063131daf418410
devacunetixtech <adebisiprecious780@gmail.com>
2023-09-22 05:43:00 +0100

    added protect

--- src/routes/group.js
+++ src/routes/group.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+const protect = require('../middlewares/protect')
 const { createGroup, updateGroup, deleteGroup} = require('../controller/group')
 
 // Post - Create group with details
-router.post('/create-group', createGroup)
+router.post('/create-group',protect,  createGroup)
 // PUT /api/group/:groupId - Update group details
 router.put('api/group/:groupId', updateGroup)
 
