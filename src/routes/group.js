cbc8abcd3819e22b0161b3e87b2afc6a341cb7a7
devacunetixtech <adebisiprecious780@gmail.com>
2023-09-22 05:41:11 +0100

    corrected routes group

--- src/routes/group.js
+++ src/routes/group.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const protect = require('../middlewares/protect')
 const { createGroup, updateGroup, deleteGroup} = require('../controller/group')
-
+// use case 
+// router.get("/", protect, createGroup) => ALL ROUTES HERE SHOULD HAVE THIS!
 // Post - Create group with details
-router.post('/create-group', createGroup)
+router.post('/create-group', protect, createGroup)
 // PUT /api/group/:groupId - Update group details
-router.put('api/group/:groupId', updateGroup)
+router.put('api/group/:groupId', protect, updateGroup)
 
-router.delete('api/group/:groupId', deleteGroup)
+router.delete('api/group/:groupId', protect, deleteGroup)
 module.exports = router;
