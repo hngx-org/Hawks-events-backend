7616c1b4e2ef55a37cf226d29ec1fdf86d983302
thereson <gerardokolie09@gmail.com>
2023-09-21 21:48:37 -0700

    updated  file not in my branch

--- src/controller/comment.js
+++
@@ -1,9 +0,0 @@
-const Comment = require('../models/comment')
-const { Op } = require('sequelize');
-// Import your Sequelize models
-
-
-module.exports = {
-//   likeComment,
-//   dislikeComments,
-};
--- src/routes/comments.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const eventController = require('../controller/comment');
-
-
-
-
-module.exports = router
