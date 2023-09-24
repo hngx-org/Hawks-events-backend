4065af0f3f67d7775cb62952f305aecc045e66ae
nitoc <daren2track@gmail.com>
2023-09-24 11:12:21 +0100

    chores: refactor and fix create comment controller, add getall comment and change model for comments

--- src/controller/comment.js
+++ src/controller/comment.js
@@ -5,10 +5,10 @@ const { Op } = require("sequelize");
 // Create a new comment
 const createComment = async (req, res) => {
   try {
-    const { body, user_id, event_id } = req.body;
+    const { body, event_id, user_id } = req.body;
 
     // Create the comment in the database using the Comment model
-    const newComment = await Comment.create({ body, user_id, event_id });
+    const newComment = await Comment.create({ body, event_id, user_id });
 
     // Respond with the newly created comment
     res.status(201).json({ message: "Comment created", comment: newComment });
@@ -19,8 +19,23 @@ const createComment = async (req, res) => {
   }
 };
 
+const getAllComments = async (req, res, next) => {
+  try {
+    const events = await Comment.findAll();
+    
+    // if(!events){
+      // HANDLE SUCH CASES 
+    // }
+    res.status(200).json(events);
+  } catch (err) {
+    throw new ServerError(MESSAGES.INTERNAL_SERVER_ERROR);
+  }
+};
+
+
 module.exports = {
   createComment,
+  getAllComments
   //   likeComment,
   //   dislikeComments,
 };
